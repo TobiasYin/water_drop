fef2453622413470ea0996e946be6f002eeb865d
yinruitao.tobias <yinruitao.tobias@bytedance.com>
2020-05-29 00:36:34 +0800

    Init gin.
    Change-Id: Ia6dc49687313a3fd499b02e63ff114b8d1da3333

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/TobiasYin/water_drop
+
+go 1.14
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main()  {
+	r := gin.New()
+
+	r.GET("/", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"msg": "hello",
+		})
+	})
+
+	if err := r.Run(); err != nil{
+		panic(err)
+	}
+}
