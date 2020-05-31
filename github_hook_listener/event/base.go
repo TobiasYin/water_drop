361637e24efc1600e0390e54fb3ebc21fe348ee7
yinruitao.tobias <yinruitao.tobias@bytedance.com>
2020-05-31 15:59:15 +0800

    Update Fields.
    Change-Id: If98e6eaf3dd98d745b1f1a32c8dae376a6815f62

--- github_hook_listener/event/base.go
+++ github_hook_listener/event/base.go
@@ -11,33 +11,11 @@ type Event struct {
 	Before     string `json:"before"`
 	After      string `json:"after"`
 	Repository struct {
-		Id       int    `json:"id"`
-		NodeId   string `json:"node_id"`
-		Name     string `json:"name"`
-		FullName string `json:"full_name"`
-		Private  bool   `json:"private"`
-		Owner    struct {
-			Name              string `json:"name"`
-			Email             string `json:"email"`
-			Login             string `json:"login"`
-			Id                int    `json:"id"`
-			NodeId            string `json:"node_id"`
-			AvatarUrl         string `json:"avatar_url"`
-			GravatarId        string `json:"gravatar_id"`
-			Url               string `json:"url"`
-			HtmlUrl           string `json:"html_url"`
-			FollowersUrl      string `json:"followers_url"`
-			FollowingUrl      string `json:"following_url"`
-			GistsUrl          string `json:"gists_url"`
-			StarredUrl        string `json:"starred_url"`
-			SubscriptionsUrl  string `json:"subscriptions_url"`
-			OrganizationsUrl  string `json:"organizations_url"`
-			ReposUrl          string `json:"repos_url"`
-			EventsUrl         string `json:"events_url"`
-			ReceivedEventsUrl string `json:"received_events_url"`
-			Type              string `json:"type"`
-			SiteAdmin         bool   `json:"site_admin"`
-		} `json:"owner"`
+		Id               int         `json:"id"`
+		NodeId           string      `json:"node_id"`
+		Name             string      `json:"name"`
+		FullName         string      `json:"full_name"`
+		Private          bool        `json:"private"`
 		HtmlUrl          string      `json:"html_url"`
 		Description      interface{} `json:"description"`
 		Fork             bool        `json:"fork"`
@@ -112,69 +90,11 @@ type Event struct {
 		Name  string `json:"name"`
 		Email string `json:"email"`
 	} `json:"pusher"`
-	Sender struct {
-		Login             string `json:"login"`
-		Id                int    `json:"id"`
-		NodeId            string `json:"node_id"`
-		AvatarUrl         string `json:"avatar_url"`
-		GravatarId        string `json:"gravatar_id"`
-		Url               string `json:"url"`
-		HtmlUrl           string `json:"html_url"`
-		FollowersUrl      string `json:"followers_url"`
-		FollowingUrl      string `json:"following_url"`
-		GistsUrl          string `json:"gists_url"`
-		StarredUrl        string `json:"starred_url"`
-		SubscriptionsUrl  string `json:"subscriptions_url"`
-		OrganizationsUrl  string `json:"organizations_url"`
-		ReposUrl          string `json:"repos_url"`
-		EventsUrl         string `json:"events_url"`
-		ReceivedEventsUrl string `json:"received_events_url"`
-		Type              string `json:"type"`
-		SiteAdmin         bool   `json:"site_admin"`
-	} `json:"sender"`
 	Created bool        `json:"created"`
 	Deleted bool        `json:"deleted"`
 	Forced  bool        `json:"forced"`
 	BaseRef interface{} `json:"base_ref"`
 	Compare string      `json:"compare"`
-	Commits struct {
-		Id        string `json:"id"`
-		TreeId    string `json:"tree_id"`
-		Distinct  bool   `json:"distinct"`
-		Message   string `json:"message"`
-		Timestamp string `json:"timestamp"`
-		Url       string `json:"url"`
-		Author    struct {
-			Name  string `json:"name"`
-			Email string `json:"email"`
-		} `json:"author"`
-		Committer struct {
-			Name  string `json:"name"`
-			Email string `json:"email"`
-		} `json:"committer"`
-		Added    []string      `json:"added"`
-		Removed  []interface{} `json:"removed"`
-		Modified []interface{} `json:"modified"`
-	} `json:"commits"`
-	HeadCommit struct {
-		Id        string `json:"id"`
-		TreeId    string `json:"tree_id"`
-		Distinct  bool   `json:"distinct"`
-		Message   string `json:"message"`
-		Timestamp string `json:"timestamp"`
-		Url       string `json:"url"`
-		Author    struct {
-			Name  string `json:"name"`
-			Email string `json:"email"`
-		} `json:"author"`
-		Committer struct {
-			Name  string `json:"name"`
-			Email string `json:"email"`
-		} `json:"committer"`
-		Added    []string      `json:"added"`
-		Removed  []interface{} `json:"removed"`
-		Modified []interface{} `json:"modified"`
-	} `json:"head_commit"`
 }
 
 var (
